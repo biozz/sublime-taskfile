fd4152f02e5ace957204b6cc8531ca029e9a56ff
Ivan Elfimov <ielfimov@gmail.com>
2021-05-27 10:44:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sublime Taskfile
+
+A Sublime Text 4 plugin for running tasks from [Taskfile](https://taskfile.dev). It adds `Run Task` to command palette and you can select which task to run. The output of the task is than displayed in the quick panel on the bottom.
+
+## Caveats
+
+- made for Sublime Text version 4
+- uses Sublime's python version 3.8
+- has a vendored package - PyYAML
+- only officially supports MaxOS X
---
+++ Taskfile.py
@@ -0,0 +1,24 @@
+import os
+import zipimport
+
+from pathlib import Path
+from functools import partial
+
+import sublime_plugin
+
+
+cwd = Path(__file__).parent
+pack_path = cwd / 'vendor/PyYAML-5.4.1-cp38-cp38-macosx_10_9_x86_64.whl'
+importer = zipimport.zipimporter(pack_path.resolve())
+yaml = importer.load_module('yaml')
+taskfile = open(cwd / 'Taskfile.yml')
+res = yaml.load(taskfile, Loader=yaml.FullLoader)
+tasks_keys = list([t for t in res.get('tasks')])
+
+class RunTaskCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		on_done = partial(run_task_by_index, self.window)
+		self.window.show_quick_panel(tasks_keys, on_done)
+
+def run_task_by_index(window, task_index):
+	window.run_command('exec', args={'shell_cmd': f'task {tasks_keys[task_index]}'})
